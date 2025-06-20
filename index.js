4f84069b8bb0217b4f6d257eb002beeedb3a3dcd
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2025-06-20 14:54:23 -0300

    lil bro...

--- index.js
+++ index.js
@@ -334,7 +334,7 @@ client.on("messageCreate", async (message) => {
 });
 
 // --- Cron para enviar imagem toda sexta-feira às 13h ---
-cron.schedule('40 14 * * 5', async () => {
+cron.schedule('30 15 * * 5', async () => {
   if (imagensSexta.length === 0) return;
   const sorteada = imagensSexta[Math.floor(Math.random() * imagensSexta.length)];
   try {
