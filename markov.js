77c2c891d96c0ce3ed161e60310ecc0582b44fa6
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2025-08-17 18:23:04 -0300

    implementação métodos de carregamento e salvamento da cadeia de Markov

--- markov.js
+++ markov.js
@@ -1,19 +1,23 @@
+const fs = require('fs');
+
+const CHAIN_PATH = 'markov_chain.json';
+
 class MarkovChain {
     constructor() {
         this.chain = {};
+        this._loadChain();
     }
 
     addMessage(msg) {
         const words = msg.split(/\s+/);
         try {
-            for (let i = 0; i < words.length; i++) {
+            for (let i = 0; i < words.length - 1; i++) {
                 const word = words[i].toLowerCase();
-                const next_word = words[i+1] ? words[i + 1].toLowerCase() : null;;
+                const next_word = words[i + 1] ? words[i + 1].toLowerCase() : null;
                 if (!this.chain[word]) this.chain[word] = [];
-                this.chain[word].push(next_word);
-                
+                if (next_word) this.chain[word].push(next_word);
             }
-            
+            this._saveChain();
         } catch (e) {
             console.error("erro na leitura das mensagens", e);
         }
@@ -36,5 +40,24 @@ class MarkovChain {
         return keys[Math.floor(Math.random() * keys.length)];
     }
 
+    _saveChain() {
+        try {
+            fs.writeFileSync(CHAIN_PATH, JSON.stringify(this.chain, null, 2), 'utf8');
+        } catch (e) {
+            console.error('Erro ao salvar a cadeia Markov:', e);
+        }
+    }
+
+    _loadChain() {
+        try {
+            if (fs.existsSync(CHAIN_PATH)) {
+                this.chain = JSON.parse(fs.readFileSync(CHAIN_PATH, 'utf8'));
+            }
+        } catch (e) {
+            console.error('Erro ao carregar a cadeia Markov:', e);
+            this.chain = {};
+        }
+    }
 }
+
 module.exports = MarkovChain;
